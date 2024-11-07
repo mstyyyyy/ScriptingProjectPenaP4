b9df6b8bc0a59a491387948a7e1611e512068726
mstyyyyy <722833@student.dusd.net>
2024-11-07 10:28:53 -0800

    Awake and start script
    I made a script for awake and start added a plane and cube.

---
+++ ScriptingProjectP4Pena/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    private void Awake ()
+    {
+        Debug.Log("Awake called.");
+    }
+   
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+
+   
+   
+}
